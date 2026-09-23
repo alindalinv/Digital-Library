45b152a8d06165cae185c4a6295b9d68b6dbfdb5
v.dalin <v.dalin@ardb.com.kh>
2026-09-23 10:58:36 +0700

    fix

--- resources/views/components/admin/tables/basic-tables/basic-tables-three.blade.php
+++ resources/views/components/admin/tables/basic-tables/basic-tables-three.blade.php
@@ -252,7 +252,7 @@
                                 </td>
                                 <td class="px-4 py-4 text-sm font-medium text-right whitespace-nowrap">
                                     <div class="flex justify-center relative">
-                                        <x-common.table-dropdown>
+                                        <x-admin.common.table-dropdown>
                                             <x-slot name="button">
                                                 <button type="button" id="options-menu" aria-haspopup="true" aria-expanded="true" class="text-gray-500'">
                                                     <svg class="fill-current" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" clip-rule="evenodd" d="M5.99902 10.245C6.96552 10.245 7.74902 11.0285 7.74902 11.995V12.005C7.74902 12.9715 6.96552 13.755 5.99902 13.755C5.03253 13.755 4.24902 12.9715 4.24902 12.005V11.995C4.24902 11.0285 5.03253 10.245 5.99902 10.245ZM17.999 10.245C18.9655 10.245 19.749 11.0285 19.749 11.995V12.005C19.749 12.9715 18.9655 13.755 17.999 13.755C17.0325 13.755 16.249 12.9715 16.249 12.005V11.995C16.249 11.0285 17.0325 10.245 17.999 10.245ZM13.749 11.995C13.749 11.0285 12.9655 10.245 11.999 10.245C11.0325 10.245 10.249 11.0285 10.249 11.995V12.005C10.249 12.9715 11.0325 13.755 11.999 13.755C12.9655 13.755 13.749 12.9715 13.749 12.005V11.995Z" fill="currentColor" />
@@ -268,7 +268,7 @@
                                                     Delete
                                                 </a>
                                             </x-slot>
-                                        </x-common.table-dropdown>
+                                        </x-admin.common.table-dropdown>
                                     </div>
                                 </td>
                             </tr>
