c951cb2e038ff7e14c4e7755c29e8db76e298323
v.dalin <v.dalin@ardb.com.kh>
2026-09-25 14:11:58 +0700

    improve

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -10,14 +10,12 @@
          @keydown.escape.window="openProfileHeaderModal = false; openProfileInfoModal = false">
 
 
-        <div class="rounded-2xl border border-gray-200 bg-white p-5 lg:p-6 dark:border-gray-800 dark:bg-gray-900">
             <h3 class="mb-5 text-lg font-semibold text-gray-800 lg:mb-7 dark:text-white/90">
                 Profile
             </h3>
 
             <x-admin.profile.profile-card :user="$user" />
             <x-admin.profile.personal-info-card :user="$user" />
-        </div>
 
         {{-- Modals INSIDE the Alpine scope --}}
         <x-admin.profile.profile-header-modal :user="$user" />
